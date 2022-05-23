67fd5c5ad37a49ab2f379995c32f80342f36e737
Kevin Sana <kevin.sana@outlook.com>
2022-05-23 16:46:18 -0500

    added functionality to delete and update reminders on tasks

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
 
 
    async addTask(task) {
-     const res = await fetch('/api/tasks', {
+     const res = await fetch('api/tasks', {
        method: 'POST',
        headers: {
          'Content-type': 'application/json',
@@ -51,15 +51,34 @@ export default {
     },
 
 
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error Deleting Task')
+        
       }
     },
 
 
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(updateTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
     },
 
 
@@ -72,7 +91,7 @@ export default {
 
 
     async fetchTask(id) {
-      const res = await fetch(`api/${id}`)
+      const res = await fetch(`api/tasks/${id}`)
       const data = await res.json()
 
       return data
